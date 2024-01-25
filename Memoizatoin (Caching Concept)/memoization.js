cb8a8b50f8572ca77a482ddc38bb26163b149835
Aparnaraha <aparnaraha2912@gmail.com>
2024-01-26 00:03:13 +0530

    memoization updated

---
+++ Memoizatoin (Caching Concept)/memoization.js
@@ -0,0 +1,11 @@
+/*  
+
+Implementation of Memoization in Javascript
+
+Memoization is an optimization technique that can be used to reduce time-consuming calculations by saving previous input to something called cache and returning result from it.
+
+
+It makes applications more efficient and hence faster.
+*/
+
+let sum = 0;
